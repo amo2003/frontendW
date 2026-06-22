5ed8a6b6e5eb612facf6c1aafee0a558a5163ee3
Amod Indupa <amodindupa9@gmail.com>
2026-06-22 21:04:46 +0530

    add

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ import { AuthProvider } from '@asgardeo/auth-react';
 import App from './App';
 import './index.css';
 
+const BASE_URL = process.env.REACT_APP_ASGARDEO_BASE_URL;
+
 const authConfig = {
-  signInRedirectURL: 'http://localhost:3000',
-  signOutRedirectURL: 'http://localhost:3000',
+  signInRedirectURL: window.location.origin,
+  signOutRedirectURL: window.location.origin,
   clientID: process.env.REACT_APP_ASGARDEO_CLIENT_ID,
-  baseUrl: process.env.REACT_APP_ASGARDEO_BASE_URL,
+  baseUrl: BASE_URL,
   scope: ['openid', 'profile', 'email'],
+  endpoints: {
+    authorizationEndpoint: `${BASE_URL}/oauth2/authorize`,
+    tokenEndpoint: `${BASE_URL}/oauth2/token`,
+    endSessionEndpoint: `${BASE_URL}/oidc/logout`,
+    jwksUri: `${BASE_URL}/oauth2/jwks`,
+    revocationEndpoint: `${BASE_URL}/oauth2/revoke`,
+    checkSessionIframe: `${BASE_URL}/oidc/checksession`,
+    wellKnownEndpoint: `${BASE_URL}/oauth2/token/.well-known/openid-configuration`,
+  },
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
